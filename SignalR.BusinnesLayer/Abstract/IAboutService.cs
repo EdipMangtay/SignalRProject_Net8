1b24f8cad95e9c990267e07c7226a922566f464f
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-02-28 11:59:31 +0300

    BusinessLayer Abstract Services

---
+++ SignalR.BusinnesLayer/Abstract/IAboutService.cs
@@ -0,0 +1,13 @@
+﻿using SignalRApi.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinnesLayer.Abstract
+{
+    public interface IAboutService : IGenericService<About>
+    {
+    }
+}
---
+++ SignalR.BusinnesLayer/Abstract/ITestimonialService.cs
@@ -0,0 +1,14 @@
+﻿using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinnesLayer.Abstract
+{
+    public interface ITestimonialService : IGenericService<Testimonial>
+    {
+
+    }
+}
