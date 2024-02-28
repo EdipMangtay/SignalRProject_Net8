e35b86917229f67bcfea94c8d1533330fc9d12b6
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-02-28 13:14:08 +0300

    Manager Classes Added

---
+++ SignalR.BusinnesLayer/Concrete/DiscountManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SignalR.BusinnesLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalRApi.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class DiscountManager : IDiscoutService
+    {
+        private readonly IDiscountDal _discountDal;
+
+        public DiscountManager(IDiscountDal discountDal)
+        {
+            _discountDal = discountDal;
+        }
+
+        public void TAdd(Discount entity)
+        {
+            _discountDal.Add(entity);
+
+        }
+
+        public void TDelete(Discount entity)
+        {
+            _discountDal.Delete(entity);
+
+        }
+
+        public Discount TGetByID(int id)
+        {
+            return _discountDal.GetByID(id);
+
+        }
+
+        public List<Discount> TGetListAll()
+        {
+            return _discountDal.GetListAll();
+
+        }
+
+        public void TUpdate(Discount entity)
+        {
+            _discountDal.Update(entity);
+
+        }
+    }
+}
--- SignalR.DataAccessLayer/Abstract/ICategoryDal.cs
+++ SignalR.DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SignalR.DataAccessLayer.Abstract
 {
-    internal interface ICategoryDal : IGenericDal<Category>
+    public interface ICategoryDal : IGenericDal<Category>
     {
 
     }
