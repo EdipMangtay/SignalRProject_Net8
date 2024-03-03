f029bd2faf9d2f6c3f5482a87595994de6026651
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-03-04 00:00:18 +0300

    Added/Footer/SideBar/Navbar

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutFooterComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutFooterComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+
+		}
+
+
+	}
+}
