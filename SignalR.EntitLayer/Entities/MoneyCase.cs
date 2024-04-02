e1a12a3625f5b302b08af1a00abbe007913757b0
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-04-02 21:55:17 +0300

    .

---
+++ SignalR.EntitLayer/Entities/MoneyCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class MoneyCase
+    {
+        public int MoneyCaseID { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+
+}
