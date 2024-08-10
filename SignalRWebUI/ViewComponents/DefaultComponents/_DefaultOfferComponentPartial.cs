30e5ae7557247a6e1f27da6df819f94b71f128ba
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-08-11 01:16:08 +0300

    Offer

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.ViewModels.Dtos.DiscountDtos;
+using SignalRWebUI.ViewModels.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() // This method is called when the view component is invoked
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7272/api/Discount");
+
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+            return View(values);
+
+
         }
     }
 }
