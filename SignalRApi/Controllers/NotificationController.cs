64e31b38ba31f5c31716bfc95a651baac318e066
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-08-17 17:32:19 +0300

    error405 for notification update

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -29,6 +31,47 @@ namespace SignalRApi.Controllers
         public IActionResult GetAllNotificationByFalse()
         {
             return Ok(_notificationService.TGetAllNotificationByFalse()); // return ok ile notificationları döndürüyoruz
+        }
+        [HttpPost]
+        public IActionResult CreateNotification(CreataNotificationDto creataNotificationDto)
+        {
+            Notification notification = new Notification
+            {
+                Type = creataNotificationDto.Type,
+                Icon = creataNotificationDto.Icon,
+                Description = creataNotificationDto.Description,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Status = false
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Ekleme işlemi başarılı");
+        }
+        [HttpDelete]
+       public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Silme işlemi başarılı");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            return Ok(_notificationService.TGetByID(id));
+        }
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Type = updateNotificationDto.Type,
+                Icon = updateNotificationDto.Icon,
+                Description = updateNotificationDto.Description,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Status = updateNotificationDto.Status,
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncelleme işlemi başarılı");
         }
 
     }
---
+++ SignalRWebUI/Controllers/NotificationsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.ViewModels.Dtos.NotificationDtos;
+using System.Net.Http;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public NotificationsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7272/api/Notification");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateNotification()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateNotification(CreataNotificationDto creataNotification)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(creataNotification);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7272/api/Notification", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteNotification(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7272/api/Notification/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateNotification(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7272/api/Notification/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateNotificationDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateNotificationDto(UpdateNotificationDto updateNotification)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateNotification);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7272/api/Notification/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
